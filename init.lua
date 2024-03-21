bf3735dc6a04c9d86c902d1f75fd7b3df00635d2
Andrew DeVries <andrew@andrewdevries.net>
2024-03-20 20:16:59 -0500

    intial commit

---
+++ init.lua
@@ -0,0 +1,48 @@
+vim.cmd.set("expandtab")
+vim.cmd.set("tabstop=4")
+vim.cmd.set("softtabstop=4")
+vim.cmd.set("shiftwidth=4")
+vim.cmd.set("termguicolors")
+vim.cmd.set("nu")
+vim.cmd.set("relativenumber")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    "NLKNguyen/papercolor-theme",
+
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = { "c", "rust", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },  
+        })
+    end,
+
+    {
+        'nvim-telescope/telescope.nvim', 
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+
+},{})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
