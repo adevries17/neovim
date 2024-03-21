2e25f915b497b1cd4fecf4dd0671da0d83d8c7d5
Andrew DeVries <andrew@andrewdevries.net>
2024-03-21 11:07:40 -0500

    fix ansible lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,10 @@ return {
                 ensure_installed = {
                     "rust_analyzer",
                     "lua_ls",
+                    "ansiblels",
+                    "terraformls",
+                    "docker_compose_language_service",
+                    "nil_ls", -- nix
                 }
             })
         end
@@ -21,6 +25,10 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.rust_analyzer.setup({})
+            lspconfig.ansiblels.setup({})
+            lspconfig.terraformls.setup({})
+            lspconfig.docker_compose_language_service.setup({})
+            lspconfig.nil_ls.setup({})
         end
     }
 }
