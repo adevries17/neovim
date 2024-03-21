f3eeae391de85a8c60fe01267f5e7e62fcb1029b
Andrew DeVries <adevries@rhc.net>
2024-03-21 14:10:39 -0500

    add some more settings

--- lua/set.lua
+++ lua/set.lua
@@ -1,9 +1,27 @@
 vim.g.mapleader = " "
-vim.cmd.set("expandtab")
-vim.cmd.set("tabstop=4")
-vim.cmd.set("softtabstop=4")
-vim.cmd.set("shiftwidth=4")
-vim.cmd.set("termguicolors")
-vim.cmd.set("nu")
-vim.cmd.set("relativenumber")
+
+-- set up line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- tabs and spaces settings
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false -- disable word wrap
+vim.opt.termguicolors = true
+
 vim.keymap.set('n', '<leader>pe', vim.cmd.Ex, {})
+
+-- search settings
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- set a scroll offset
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
