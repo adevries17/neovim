2a2b9096a0ffefcbe059de6ff936286fa7c03415
Andrew DeVries <andrew@andrewdevries.net>
2024-04-01 20:11:29 -0500

    theme to nordic

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-    "sainnhe/everforest",
+    "AlexvZyl/nordic.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         -- load the colorscheme here
-        vim.cmd.colorscheme([[everforest]])
+        vim.cmd.colorscheme([[nordic]])
     end,
 }
