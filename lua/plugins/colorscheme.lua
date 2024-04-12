0e3c081bb9cb334a5b28e2e4c21a5342321efeaf
Andrew DeVries <andrew@andrewdevries.net>
2024-04-11 19:13:18 -0500

    base 16 dark mode!

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-    "nordtheme/vim",
+    "tinted-theming/base16-vim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         -- load the colorscheme here
-        vim.cmd.colorscheme([[nord]])
+        vim.cmd.colorscheme([[base16-classic-dark]])
     end,
 }
